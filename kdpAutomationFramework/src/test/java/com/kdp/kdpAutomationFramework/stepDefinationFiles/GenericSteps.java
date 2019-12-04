f01bc13720f681b8a7544fa1c3f28070b1751161
kuldeep kumar <kuldeep.kdp@gmail.com>
2019-12-04 17:51:53 +0530

    Applied proper wait for click and sendKey methods

--- kdpAutomationFramework/src/test/java/com/kdp/kdpAutomationFramework/stepDefinationFiles/GenericSteps.java
+++ kdpAutomationFramework/src/test/java/com/kdp/kdpAutomationFramework/stepDefinationFiles/GenericSteps.java
@@ -23,15 +23,29 @@ public class GenericSteps {
 	public void user_navigates_to_the_ABC_page_tab(String pageName, String ignore) throws Throwable {
         CommonFunction.navigateToPage(driver,pageName);
 	}
+	
+	@When("user is now on the (.*) (page|tab)")
+	public void nowOnThePage(String pageName, String ignore) throws Throwable {
+        CommonFunction.assertOnPage(driver,pageName);
+	}
 
 	@When("user sets the (.*) field as (.*)")
 	public void setFieldValue(String element, String value) throws Throwable {
-		CommonFunction.SendKeys(driver, element, value);
+		CommonFunction.sendKeys(driver, element, value);
+	}
+	
+	@When("user clicks on the (.*) element")
+	public void click(String element) throws Throwable {
+		CommonFunction.click(driver, element);
 	}
 	
 	@When("user clicks on the (.*) button")
-	public void clickOnButton(String element) throws Throwable {
-		CommonFunction.clickOnButton(driver, element);
+	public void clickOnButton(String buttonName) throws Throwable {
+		CommonFunction.clickOnButton(driver, buttonName);
 	}
-
+	
+	@When("user waits until the (.*) element is clickable")
+	public void waitsUntilElementIsClickable(String element) throws Throwable {
+		CommonFunction.waitForElementToBeClickable(driver, element);
+	}
 }
